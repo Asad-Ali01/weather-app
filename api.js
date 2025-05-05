05ee16ab132fa744e402fe8dabb81101d4aa9ec1
Asad-Ali01 <mail.asadali01@gmail.com>
2025-05-05 15:37:59 +0500

    Update api.js

--- api.js
+++ api.js
@@ -16,6 +16,206 @@ const windDirection = document.querySelector('.windDirection')
 const atmosphericCondition = document.querySelector('.atmosphericCondition');
 const weatherImg = document.querySelector('.weatherImg');
 
+
+const countries = {
+    "Afghanistan": "AF",
+    "Albania": "AL",
+    "Algeria": "DZ",
+    "Andorra": "AD",
+    "Angola": "AO",
+    "Argentina": "AR",
+    "Armenia": "AM",
+    "Australia": "AU",
+    "Austria": "AT",
+    "Azerbaijan": "AZ",
+    "Bahamas": "BS",
+    "Bahrain": "BH",
+    "Bangladesh": "BD",
+    "Barbados": "BB",
+    "Belarus": "BY",
+    "Belgium": "BE",
+    "Belize": "BZ",
+    "Benin": "BJ",
+    "Bhutan": "BT",
+    "Bolivia": "BO",
+    "Bosnia and Herzegovina": "BA",
+    "Botswana": "BW",
+    "Brazil": "BR",
+    "Brunei": "BN",
+    "Bulgaria": "BG",
+    "Burkina Faso": "BF",
+    "Burundi": "BI",
+    "Cambodia": "KH",
+    "Cameroon": "CM",
+    "Canada": "CA",
+    "Cape Verde": "CV",
+    "Central African Republic": "CF",
+    "Chad": "TD",
+    "Chile": "CL",
+    "China": "CN",
+    "Colombia": "CO",
+    "Comoros": "KM",
+    "Congo (Brazzaville)": "CG",
+    "Congo (Kinshasa)": "CD",
+    "Costa Rica": "CR",
+    "Croatia": "HR",
+    "Cuba": "CU",
+    "Cyprus": "CY",
+    "Czech Republic": "CZ",
+    "Denmark": "DK",
+    "Djibouti": "DJ",
+    "Dominica": "DM",
+    "Dominican Republic": "DO",
+    "Ecuador": "EC",
+    "Egypt": "EG",
+    "El Salvador": "SV",
+    "Equatorial Guinea": "GQ",
+    "Eritrea": "ER",
+    "Estonia": "EE",
+    "Eswatini": "SZ",
+    "Ethiopia": "ET",
+    "Fiji": "FJ",
+    "Finland": "FI",
+    "France": "FR",
+    "Gabon": "GA",
+    "Gambia": "GM",
+    "Georgia": "GE",
+    "Germany": "DE",
+    "Ghana": "GH",
+    "Greece": "GR",
+    "Grenada": "GD",
+    "Guatemala": "GT",
+    "Guinea": "GN",
+    "Guinea-Bissau": "GW",
+    "Guyana": "GY",
+    "Haiti": "HT",
+    "Honduras": "HN",
+    "Hungary": "HU",
+    "Iceland": "IS",
+    "India": "IN",
+    "Indonesia": "ID",
+    "Iran": "IR",
+    "Iraq": "IQ",
+    "Ireland": "IE",
+    "Israel": "IL",
+    "Italy": "IT",
+    "Jamaica": "JM",
+    "Japan": "JP",
+    "Jordan": "JO",
+    "Kazakhstan": "KZ",
+    "Kenya": "KE",
+    "Kiribati": "KI",
+    "Kuwait": "KW",
+    "Kyrgyzstan": "KG",
+    "Laos": "LA",
+    "Latvia": "LV",
+    "Lebanon": "LB",
+    "Lesotho": "LS",
+    "Liberia": "LR",
+    "Libya": "LY",
+    "Liechtenstein": "LI",
+    "Lithuania": "LT",
+    "Luxembourg": "LU",
+    "Madagascar": "MG",
+    "Malawi": "MW",
+    "Malaysia": "MY",
+    "Maldives": "MV",
+    "Mali": "ML",
+    "Malta": "MT",
+    "Marshall Islands": "MH",
+    "Mauritania": "MR",
+    "Mauritius": "MU",
+    "Mexico": "MX",
+    "Micronesia": "FM",
+    "Moldova": "MD",
+    "Monaco": "MC",
+    "Mongolia": "MN",
+    "Montenegro": "ME",
+    "Morocco": "MA",
+    "Mozambique": "MZ",
+    "Myanmar": "MM",
+    "Namibia": "NA",
+    "Nauru": "NR",
+    "Nepal": "NP",
+    "Netherlands": "NL",
+    "New Zealand": "NZ",
+    "Nicaragua": "NI",
+    "Niger": "NE",
+    "Nigeria": "NG",
+    "North Korea": "KP",
+    "North Macedonia": "MK",
+    "Norway": "NO",
+    "Oman": "OM",
+    "Pakistan": "PK",
+    "Palau": "PW",
+    "Palestine": "PS",
+    "Panama": "PA",
+    "Papua New Guinea": "PG",
+    "Paraguay": "PY",
+    "Peru": "PE",
+    "Philippines": "PH",
+    "Poland": "PL",
+    "Portugal": "PT",
+    "Qatar": "QA",
+    "Romania": "RO",
+    "Russia": "RU",
+    "Rwanda": "RW",
+    "Saint Kitts and Nevis": "KN",
+    "Saint Lucia": "LC",
+    "Saint Vincent and the Grenadines": "VC",
+    "Samoa": "WS",
+    "San Marino": "SM",
+    "Sao Tome and Principe": "ST",
+    "Saudi Arabia": "SA",
+    "Senegal": "SN",
+    "Serbia": "RS",
+    "Seychelles": "SC",
+    "Sierra Leone": "SL",
+    "Singapore": "SG",
+    "Slovakia": "SK",
+    "Slovenia": "SI",
+    "Solomon Islands": "SB",
+    "Somalia": "SO",
+    "South Africa": "ZA",
+    "South Korea": "KR",
+    "South Sudan": "SS",
+    "Spain": "ES",
+    "Sri Lanka": "LK",
+    "Sudan": "SD",
+    "Suriname": "SR",
+    "Sweden": "SE",
+    "Switzerland": "CH",
+    "Syria": "SY",
+    "Taiwan": "TW",
+    "Tajikistan": "TJ",
+    "Tanzania": "TZ",
+    "Thailand": "TH",
+    "Timor-Leste": "TL",
+    "Togo": "TG",
+    "Tonga": "TO",
+    "Trinidad and Tobago": "TT",
+    "Tunisia": "TN",
+    "Turkey": "TR",
+    "Turkmenistan": "TM",
+    "Tuvalu": "TV",
+    "Uganda": "UG",
+    "Ukraine": "UA",
+    "United Arab Emirates": "AE",
+    "United Kingdom": "GB",
+    "United States": "US",
+    "Uruguay": "UY",
+    "Uzbekistan": "UZ",
+    "Vanuatu": "VU",
+    "Vatican City": "VA",
+    "Venezuela": "VE",
+    "Vietnam": "VN",
+    "Yemen": "YE",
+    "Zambia": "ZM",
+    "Zimbabwe": "ZW"
+  }
+  
+
+
 export async function getWeatherData({cityValue,countryValue,latitudeValue,longitudeValue}) {
 
     try{
@@ -24,14 +224,16 @@ export async function getWeatherData({cityValue,countryValue,latitudeValue,longi
         })
         zip.classList.add('active');
         showloader();
-        // console.log(latitudeValue);
-        // console.log(longitudeValue);
+
         
         let response;
         if(cityValue && countryValue){
-            response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${apikey}&units=metric`
-            );
+         
+                response = await fetch(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${apikey}&units=metric`
+                );
+        
+           
         }else if(latitudeValue && longitudeValue){
             response = await fetch(
                 `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeValue}&lon=${longitudeValue}&appid=${apikey}&units=metric`
@@ -40,12 +242,19 @@ export async function getWeatherData({cityValue,countryValue,latitudeValue,longi
     
         if(!response.ok){
             console.log("Api error");
-           
+           alert("Api error");
+           hideloader();
             return;
         }
      
         const data = await response.json()
-
+        if(cityValue && countryValue){
+            if(data.sys.country !== countries[countryValue]){
+                alert("Api error")
+                hideloader();
+            }
+        }
+       
         weatherColRight.style.display = 'flex';  //display the right col
 
         hideloader()
@@ -54,8 +263,8 @@ export async function getWeatherData({cityValue,countryValue,latitudeValue,longi
     }
     catch(err){
         console.log("Fetch failed or API error:", err.message);     
-        const simulate = await simulateData(); // 👈 fallback fake data
-        processWeatherData(simulate);          // 👈 display simulated data
+        const simulate = await simulateData(); 
+        processWeatherData(simulate);          
         weatherColRight.style.display = 'flex';
         hideloader();   
         alert("You're offline. Showing simulated weather data.");
@@ -83,8 +292,8 @@ function simulateData(){
                 },
                 visibility:10000,
                 weather:[{
-                        main:'Clouds',
-                        icon:"03d",
+                        main:'broken clouds',
+                        icon:"04d",
                     }],
                 wind:{
                     speed:2.57,
